2cefb36d0168d4cd1de461f849ec328c6646ab85
babyshoes <dianecai@gmail.com>
2015-08-03 13:52:41 -0400

    all done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+katz_deli = ["Logan", "Avi", "Spencer"]
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.index(name)+1} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+  	puts "There is nobody waiting to be served!" 
+  else
+  	person = deli.shift
+  	puts "Currently serving #{person}."
+  end
+end
+
+def line(deli)
+  if deli.empty?
+  	puts "The line is currently empty."
+  else
+  	ordered_line = deli.map {|name| "#{deli.index(name) + 1}. #{name}"}
+  	puts "The line is currently: #{ordered_line.join(" ")}"
+  	# deli.each {|name| print " #{deli.index(name) + 1}. #{name}"}
+  end
+end
+
+take_a_number(katz_deli, "Diane")
+line(katz_deli)
